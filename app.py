4814350ed6ac6439a17ef3c3fffadefae3e883db
faizakanwal0321-ux <faizakanwal0321@gmail.com>
2026-09-08 23:07:19 +0600

    Update app.py

--- app.py
+++ app.py
@@ -212,7 +212,7 @@ ANSWER:
 
     response = client.chat.completions.create(
 
-        model="model="llama-3.3-70b-versatile",
+        model="llama-3.3-70b-versatile",
 
         messages=[
             {
